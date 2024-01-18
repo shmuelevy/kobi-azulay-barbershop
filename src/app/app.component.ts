f182f428b3c34ae2597d918a78e8c131feb937fd
Shmuel Levy <shmuelev1234@gmail.com>
2024-01-18 05:11:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'barbershop-app';
+
+  clientsList:any[]=[]
+
+
+
+constructor( private http:HttpClient){}
+
+getData(){
+   this.http.get('https://kobi--azulay-default-rtdb.firebaseio.com/clients.json').subscribe(users=>{  
+  console.log('users: ',users) 
+  this.clientsList.push(users)
+})
+}
+
+
+ngOnInit(): void {
+  for(let clients of this.clientsList){
+    console.log('clients: ',clients) 
+  }
+}
+
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  fullName:string=''
+  phoneNumber:any;
+  selectedBarber:string=''
+  selectedDay:string=''
+  selectedHour:string=''
+    days:any[]=[
+      '14/1/24-יום ראשון',
+      '15/1/24-יום שני',
+      '16/1/24-יום שלישי',
+      '17/1/24-יום רביעי',
+      '18/1/24-יום חמישי',
+    ]
+  
+  hours:any[]=[
+    '10:00',
+    '10:30',
+    '11:00',
+    '11:30',
+    '12:00',
+    '12:30',
+    '13:00',
+    '13:30',
+    '14:00',
+    '14:30',
+    '15:00',
+    '15:30',
+    '16:00',
+    '16:30',
+    '17:00',
+    '17:30',
+    '18:00',
+    '18:30',
+    '19:00',
+    '19:30',
+    '20:00',
+    '20:30',
+    '21:00',
+    '21:30',
+  ]
+  
+  
+  approve():void{
+    if(this.selectedHour&&this.fullName&&this.phoneNumber){
+    alert(`${this.fullName} קבע תור עם ${this.selectedBarber} ביום ${this.selectedDay} בשעה ${this.selectedHour}`)
+    const indexToRemove = this.hours.indexOf(this.selectedHour);
+      if (indexToRemove !== -1) {
+        this.hours.splice(indexToRemove, 1);
+      }
+    this.phoneNumber='';
+    this.selectedBarber=''
+    this.selectedDay=''
+    this.selectedHour=''
+    this.fullName=''
+  }
+  }
+}
