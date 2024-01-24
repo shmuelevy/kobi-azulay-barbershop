fd18b174ba5635f3311e0429676e47fc833e0840
Shmuel Levy <shmuelev1234@gmail.com>
2024-01-24 04:30:47 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
-    {path:'admin',component:AdminComponent}
+    {path:'admin',component:AdminComponent},
+    {path:'**',component:PageNotFoundComponent},
 ];
